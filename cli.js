f497e7336e8ce02bffd92809479279a91b0be64c
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-28 11:51:39 +0100

    done with 13.3

---
+++ cli.js
@@ -0,0 +1,50 @@
+require("dotenv").config()
+const { Sequelize, Model, DataTypes } = require("sequelize")
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+)
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log("successfully connected")
+    const blogs = await Blog.findAll()
+    blogs.map((blog) => {
+      console.log(
+        `${blog.title} by ${blog.author}: ${blog.url} | ${blog.likes} likes`
+      )
+    })
+  } catch (error) {
+    console.error("error connecting:", error)
+  }
+}
+main()
