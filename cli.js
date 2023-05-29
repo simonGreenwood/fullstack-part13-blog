46a1dadb65dd2d1fc784e16f8fa223397a44beb5
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-29 08:04:24 +0100

    added start and dev scripts, 13.4 done

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 require("dotenv").config()
 const { Sequelize, Model, DataTypes } = require("sequelize")
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const express = require("express")
+const app = express()
+app.use(express.json())
 class Blog extends Model {}
 Blog.init(
   {
@@ -33,18 +37,26 @@ Blog.init(
   }
 )
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log("successfully connected")
-    const blogs = await Blog.findAll()
-    blogs.map((blog) => {
-      console.log(
-        `${blog.title} by ${blog.author}: ${blog.url} | ${blog.likes} likes`
-      )
-    })
-  } catch (error) {
-    console.error("error connecting:", error)
+app.get("/api/blogs/", async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(blogs.map((blog) => blog.toJSON()))
+  res.json(blogs)
+})
+
+app.post("/api/blogs/", async (req, res) => {
+  const newBlog = await Blog.create(req.body)
+  res.json(newBlog)
+})
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blogToDelete = await Blog.findByPk(req.params.id)
+  if (blogToDelete === null) {
+    return res.status(400).json({ error: "Invalid ID" })
   }
-}
-main()
+  await blogToDelete.destroy()
+  res.status(204).end()
+})
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
+})
