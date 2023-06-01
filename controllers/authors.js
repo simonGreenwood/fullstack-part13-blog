8d8735a19ed326b35159496198b26723082dc0be
simon greenwood <simonrobertgreenwood@gmail.com>
2023-06-01 08:00:55 +0100

    added the article count

--- controllers/authors.js
+++ controllers/authors.js
@@ -2,12 +2,17 @@ const router = require("express").Router()
 const jwt = require("jsonwebtoken")
 const { Blog, User } = require("../models")
 const { SECRET } = require("../utils/config")
+const { sequelize } = require("../utils/db")
 
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll({
+  const users = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn("COUNT", sequelize.col("author")), "articles"],
+    ],
     group: "author",
   })
-  res.json(blogs)
+  res.json(users)
 })
 
 module.exports = router
