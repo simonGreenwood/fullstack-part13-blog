e26c082b5638728c2d7a29c4ec8d420465470827
simon greenwood <simonrobertgreenwood@gmail.com>
2023-06-04 11:36:44 +0100

    added the token checking feature

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,18 +1,25 @@
 const { SECRET } = require("./config");
 const jwt = require("jsonwebtoken");
-const { User, Blog } = require("../models");
+const { User, Blog, Session } = require("../models");
 const tokenExtractor = async (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     try {
       const decodedToken = jwt.verify(authorization.substring(7), SECRET);
-      req.user = await User.findByPk(decodedToken.id);
+      const user = await User.findByPk(decodedToken.id);
+      const session = await Session.findAll({
+        where: { token: authorization.substring(7) },
+      });
+      if (session.length === 0 || user.disabled) {
+        return res.status(401).json({ error: "token invalid" });
+      }
+
+      req.user = user;
     } catch {
       return res.status(401).json({ error: "token invalid" });
     }
   } else {
     return res.status(401).json({ error: "token missing" });
-    a;
   }
   next();
 };
